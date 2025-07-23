cbfc53860b9a57e208e86aba3250b287574bd4e2
God'swill Sekav Apya <godswillapya@gmail.com>
2025-07-23 14:30:01 +0100

    feat: enhance image animation effects for improved user experience

--- src/sections/Image.jsx
+++ src/sections/Image.jsx
@@ -6,8 +6,10 @@ const Image = () => {
   return (
     <section className="w-full">
       <motion.img
-        initial="hidden"
-  whileInView="visible"
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 2, ease: "easeInOut" }}
+        viewport={{ once: false, amount: 0 }}
         src={secondImage}
         alt="Couple - Mary and Victor"
         className="w-full object-cover"
